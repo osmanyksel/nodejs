074498eb3ad4b0b3bd6194945ca8692d296ec7c1
osmanyksel <36537953+osmanyksel@users.noreply.github.com>
2021-08-23 14:52:48 +0300

    en son

--- src/Events-I_O/event-emitter.js
+++ src/Events-I_O/event-emitter.js
@@ -1,23 +1,22 @@
-const events=require('events');
-const eventEmitter=new events.EventEmitter();
+const events = require("events");
+const eventEmitter = new events.EventEmitter();
 
 // Event Tanımlama
-eventEmitter.on('selamla',(object)=>{
+eventEmitter.on("selamla", (object) => {
   console.log(`merhaba ${object.name} ${object.surname}`);
 });
 
 // BUnun farkı ise once sadece bir kere emitlenir.
-eventEmitter.once('merhabaDe',()=>{
+eventEmitter.once("merhabaDe", () => {
   console.log("merhaba");
 });
 
 // Event Caagirma
-eventEmitter.emit('selamla',{name:'Osman',surname:'Yüksel'});
+eventEmitter.emit("selamla", { name: "Osman", surname: "Yüksel" });
 
-setTimeout(()=>{
-  eventEmitter.emit('selamla',{name:'Murat',surname:'Kömür'});
-},1000);
+setTimeout(() => {
+  eventEmitter.emit("selamla", { name: "Murat", surname: "Kömür" });
+}, 1000);
 
-
-eventEmitter.emit('merhabaDe');
-eventEmitter.emit('merhabaDe');
+eventEmitter.emit("merhabaDe");
+eventEmitter.emit("merhabaDe");

2e2d441e5cb5793a35f599c540a9647ee3eab8d2
osmanyksel <36537953+osmanyksel@users.noreply.github.com>
2021-08-23 13:48:09 +0300

    SON

--- src/nodejsHTTP/nodejsHTTP.js
+++ src/nodejsHTTP/nodejsHTTP.js
@@ -1,17 +1,18 @@
 // Basit bir hhtp sunucusu oluşturmak
 
 const http = require("http");
-
+/*
 const server = http.createServer((request, response) => {
   console.log("bir istekde bulunuldu.");
   console.log(request.url);
   response.writeHead(200, { "content-type": "text/html;charset=utf-8" });
-  response.write("<b>OSMAN YÜKSEL</b>");
+  response.write("<b>OSMAN YÜKSEL1</b>");
   // Burada isterseniz write icine yazılanları direk end içine yazabilirsiniz.
   response.end();
 });
 
 server.listen(3000);
+*/
 
 // Mevcut bir html safyasını basma
 
@@ -21,7 +22,11 @@ const server2 = http.createServer((request, response) => {
   console.log("bir istekde bulunuldu.");
   console.log(request.url);
   response.writeHead(200, { "content-type": "text/html;charset=utf-8" });
-  fs.readFile("", (err, data) => {});
+  fs.readFile("src/nodejsHTTP/index.html", (err, data) => {
+    if (err) throw err;
+
+    response.end(data);
+  });
 });
 
 server2.listen(3001);

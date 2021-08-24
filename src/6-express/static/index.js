8d674e55ebb7e3b009ff047f437eadd17d1ce80a
osmanyksel <36537953+osmanyksel@users.noreply.github.com>
2021-08-24 15:36:11 +0300

    son

--- src/6-express/static/index.js
+++ src/6-express/static/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 // static klasör belirleme
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.render("index");

48b1ea265069caaf09a8555342a0fb3432f90b2f
osmanyksel <36537953+osmanyksel@users.noreply.github.com>
2021-08-25 14:53:16 +0300

    son

--- src/6-express/routing/routing.js
+++ src/6-express/routing/routing.js
@@ -3,14 +3,26 @@ const app = express();
 
 /*
   get,post,put,delete,all
+  ? (zorunlu olmayan) burada soru işaretindeki önceki harfi yazmasanızda sonuç size dönecektir.
 
+  * (yerine herhangi bir ifade gelebilir) Burada *koyduğunuz yere istediğiniz seyi yazabilirsiniz.
   
+  + (soldaki ifadenin aynısı olmalı) Burada + işareti koyduğumuz harfin solundaki harfden istediğimiz kadar ekleyebiliriz.
 */
 
-app.get("/iletisim/", (req, res) => {
-  res.send("merhaba express");
+app
+  .get("/ilet+isim", (req, res) => {
+    res.send("merhaba express");
+  })
+  .post("iletisim", (req, res) => {
+    res.send("iletisim post gonderildi.");
+  });
+
+// burası get,post vs. ne gönderirseniz calisacaktir.
+app.all("/all", (req, res) => {
+  res.send("all isteği calisti.");
 });
 
 app.listen(3000, () => {
-  console.log("express server çalıştı.");
+  console.log("express server calisiyor.");
 });
